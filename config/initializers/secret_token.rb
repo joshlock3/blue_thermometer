8cc8a641b908a99fb999c555d4ad94f5f455789c
joshlock3 <joshualockhart@ymail.com>
2012-02-08 20:58:27 -0600

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BlueThermometer::Application.config.secret_token = '6a6911ccc420a9fc0832f8de98f2b69b53a914c47b60dbc1337de318d95c0ae014ad9b2a8f36aa3ef6076351ad7a1c6ef6c117181bf60c36af9685f657bd9565'
