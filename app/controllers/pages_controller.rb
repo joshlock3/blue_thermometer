fa999fb2b97d9557dc579864b322a32b246ded31
Joshua Lockhart <joshuarlockhart@gmail.com>
2013-05-09 08:10:02 -0500

    minor styling changes

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,6 @@
 class PagesController < ApplicationController
   def home
+    @tracks = Track.all
   end
 
   def help
--- app/models/track.rb
+++ app/models/track.rb
@@ -1,3 +1,11 @@
 class Track < ActiveRecord::Base
   belongs_to :user
+
+  attr_accessible :name, :count, :user_id
+
+  validates :name, :presence => true, :length => {:minimum => 3}
+
+  validates :name, :presence => true
+
+  validates :user_id, :presence => true
 end
